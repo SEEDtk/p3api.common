43c395aa500346ec046d072087853bc2bda1f1d0
Bruce Parrello <bparrello@figresearch.com>
2021-04-22 12:43:54 -0500

    Snip testing.

--- src/test/java/org/theseed/p3api/common/AppTest.java
+++ src/test/java/org/theseed/p3api/common/AppTest.java
@@ -1,41 +1,31 @@
 package org.theseed.p3api.common;
 
-import junit.framework.Test;
-
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
+import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.theseed.genome.Genome;
+import org.theseed.locations.Location;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * simple test
-     */
-    public void testObjectivism() {
-        assertThat("Ayn Rand is wrong.", 'A', equalTo('A'));
+    @Test
+    public void testDnaFetch() throws IOException {
+        Genome str277 = new Genome(new File("data", "MG1655-ATCC21277.gto"));
+        Genome wild = new Genome(new File("data", "MG1655-wild.gto"));
+        Location strLoc = Location.create("NODE_2_length_269836_cov_32.5831", 9884, 9577);
+        Location wLoc = Location.create("511145.183.con.0001", 1039372, 1039679);
+        String strDna = str277.getDna(strLoc);
+        String wDna = wild.getDna(wLoc);
+        System.out.println(strDna);
+        System.out.println(wDna);
+        assertThat("A", equalTo("A"));
     }
 
 }
